3a59237fb50bd1fc2a341e02ccf8f591787a175f
pjs <pjs@bfff8976-bc45-0410-b47f-db2f775d18e7>
2011-02-13 03:21:39 +0000

    who needs pruning stats?

--- src/org/gnubridge/search/DoubleDummySolver.java
+++ src/org/gnubridge/search/DoubleDummySolver.java
@@ -19,26 +19,16 @@ public class DoubleDummySolver {
 
 	private List<Integer> finalMoves;
 
-	private int prunedAlpha;
-
-	private int prunedBeta;
-
 	private int positionsCount;
 
 	private long runningTime;
 
 	private int maxTricks = 13;
 
-	private int prunedSequence;
-
-	private int prunedPlayedSequence;
-
-	//private boolean useAlphaBetaPruning = true;
 	private boolean useDuplicateRemoval = true;
 	private boolean shouldPruneCardsInSequence = true;
 	private boolean shouldPruneCardsInPlayedSequence = true;
 
-	private int prunedDuplicatePosition;
 	PositionLookup lookup;
 
 	private boolean terminateIfRootOnlyHasOneValidMove = true;
@@ -80,29 +70,18 @@ public class DoubleDummySolver {
 
 	public void search() {
 		long start = System.currentTimeMillis();
-		initStats();
+		runningTime = 0;
+		positionsCount = 0;
 		root = new Node(null);
 		stack.push(root);
-		int i = 0;
 
 		while (!stack.empty()) {
 			Node node = stack.pop();
 			examinePosition(node);
-			collectStats(node);
-			i++;
+			positionsCount++;
 		}
 		runningTime = System.currentTimeMillis() - start;
 
-	}
-
-	private void initStats() {
-		runningTime = 0;
-		prunedAlpha = 0;
-		prunedBeta = 0;
-		prunedDuplicatePosition = 0;
-		prunedSequence = 0;
-		prunedPlayedSequence = 0;
-		positionsCount = 0;
 	}
 
 	public void setUseDuplicateRemoval(boolean b) {
@@ -119,22 +98,6 @@ public class DoubleDummySolver {
 	 */
 	public void setShouldPruneCardsInPlayedSequence(boolean b) {
 		shouldPruneCardsInPlayedSequence = b;
-	}
-
-	private void collectStats(Node node) {
-		positionsCount++;
-		if (node.isAlphaPruned()) {
-			prunedAlpha++;
-		} else if (node.isBetaPruned()) {
-			prunedBeta++;
-		} else if (node.isSequencePruned()) {
-			prunedSequence++;
-		} else if (node.isPlayedSequencePruned()) {
-			prunedPlayedSequence++;
-		} else if (node.isPrunedDuplicatePosition()) {
-			prunedDuplicatePosition++;
-		}
-
 	}
 
 	public int getPositionsExamined() {
@@ -263,38 +226,13 @@ public class DoubleDummySolver {
 		root.printOptimalPath(game);
 	}
 
-	public long getRunningTime() {
-		return runningTime;
-	}
-
-	public int getPrunedCount() {
-		return prunedAlpha + prunedBeta;
-	}
-
-	public int getPrunedAlpha() {
-		return prunedAlpha;
-	}
-
-	public int getPrunedBeta() {
-		return prunedBeta;
-	}
-
 	public void printStats() {
 		String pruneType = "Unpruned";
 		if (postEvaluationPruningStrategies.size() > 0) {
 			pruneType = "Pruned";
 		}
-		System.out.println(pruneType + " search took (msec): " + getRunningTime());
+		System.out.println(pruneType + " search took (msec): " + runningTime);
 		System.out.println("  Positions examined: " + getPositionsExamined());
-		if (postEvaluationPruningStrategies.size() > 0) {
-			System.out.println("  Alpha prunes: " + getPrunedAlpha());
-			System.out.println("  Beta prunes: " + getPrunedBeta());
-			System.out.println("  Sequence prunes: " + getPrunedSequence());
-			System.out.println("  Played Sequence prunes: " + getPrunedPlayedSequence());
-		}
-		if (useDuplicateRemoval()) {
-			System.out.println("  Duplicate position prunes: " + prunedDuplicatePosition);
-		}
 		System.out.println("West/East tricks taken: " + root.getTricksTaken(Player.WEST_EAST));
 		System.out.println("North/South tricks taken: " + root.getTricksTaken(Player.NORTH_SOUTH));
 
@@ -302,14 +240,6 @@ public class DoubleDummySolver {
 
 	public SolverConfigurator getConfigurator() {
 		return configurator;
-	}
-
-	private int getPrunedPlayedSequence() {
-		return prunedPlayedSequence;
-	}
-
-	private int getPrunedSequence() {
-		return prunedSequence;
 	}
 
 	public void setMaxTricks(int i) {
