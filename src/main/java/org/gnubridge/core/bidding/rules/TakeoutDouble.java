854de4446d6fd01796eab50aec5f23f9f427986b
Paul Slusarz <paulslusarz@carfax.com>
2025-03-16 17:13:03 -0500

    takeout double implemented but acceptance test not passing yet

--- src/main/java/org/gnubridge/core/bidding/rules/TakeoutDouble.java
+++ src/main/java/org/gnubridge/core/bidding/rules/TakeoutDouble.java
@@ -3,24 +3,53 @@ package org.gnubridge.core.bidding.rules;
 import org.gnubridge.core.Hand;
 import org.gnubridge.core.bidding.Auctioneer;
 import org.gnubridge.core.bidding.Bid;
+import org.gnubridge.core.bidding.PointCalculator;
+import org.gnubridge.core.deck.*;
+
+import java.util.*;
+
+import static org.gnubridge.core.bidding.Bid.DOUBLE;
 
 public class TakeoutDouble extends BiddingRule {
 
+	private final PointCalculator calculator;
+
 	public TakeoutDouble(Auctioneer a, Hand h) {
 		super(a, h);
-		// TODO Auto-generated constructor stub
+		calculator = new PointCalculator(hand);
 	}
 
 	@Override
 	protected boolean applies() {
-		// TODO Auto-generated method stub
-		return false;
+		int points = calculator.getCombinedPoints();
+		return auction.mayOvercall() && (
+				(13 <= points && points <= 18 && haveAtLeastCardsInAllUnbidSuits(3, auction.getEnemyTrumps())) || (points > 19));
 	}
 
 	@Override
 	protected Bid prepareBid() {
-		// TODO Auto-generated method stub
-		return null;
+		return DOUBLE;
+	}
+
+	private boolean haveAtLeastCardsInAllUnbidSuits(int minimum, Set<Trump> opponentBidSuit) {
+		List<Suit> allSuits = new ArrayList(Arrays.asList(Suit.list));
+
+		// Remove suits that opponents have bid
+		for (Trump trump : opponentBidSuit) {
+			if (trump.isSuit()) {
+				allSuits.remove(trump.asSuit());
+			}
+		}
+
+		// Check if we have at least 'minimum' cards in each unbid suit
+		for (Suit suit : allSuits) {
+			if (hand.getSuitLength(suit) < minimum) {
+				return false;
+			}
+		}
+
+		return true;
+
 	}
 
 }
--- src/test/java/org/gnubridge/core/bidding/rules/TakeoutDoubleTest.java
+++ src/test/java/org/gnubridge/core/bidding/rules/TakeoutDoubleTest.java
@@ -35,11 +35,35 @@ public class TakeoutDoubleTest extends TestCase {
 
 	}
 
-	//TODO: work here next
+
 	public void testTakeout() {
 		givenBidding(ONE_CLUBS);
 		andPlayersCards("A,K,2", "A,Q,3", "8,6,5,3", "K,J,3");
+		ruleShouldBid(DOUBLE);
+	}
+
+	public void testTakeoutNotEnoughCardsInUnbuidSuit() {
+		givenBidding(ONE_CLUBS);
+		andPlayersCards("A,K,2", "A,Q,3", "8,6", "K,J,5,3,2");
 		ruleShouldBid(null);
+	}
+
+	public void testTakeoutEnoughCardsInUnbuidSuit() {
+		givenBidding(ONE_CLUBS);
+		andPlayersCards("A,K,2", "A,Q,3", "8,6,5,3,2", "K,J");
+		ruleShouldBid(DOUBLE);
+	}
+
+	public void testTakeoutNotEnoughPoints() {
+		givenBidding(ONE_CLUBS);
+		andPlayersCards("10,8,2", "A,Q,3", "8,6,5,3,2", "K,J");
+		ruleShouldBid(null);
+	}
+
+	public void testTakeoutNotEnoughCardsInUnbuidSuitBut19Points() {
+		givenBidding(ONE_CLUBS);
+		andPlayersCards("A,K,2", "A,Q,3", "8,6", "A,K,J,5,3");
+		ruleShouldBid(DOUBLE);
 	}
 
 }
