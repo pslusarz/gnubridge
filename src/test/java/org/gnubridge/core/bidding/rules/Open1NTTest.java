f07a787cbc63d3cc224a7df468bcc57d19ed2be7
Paul Slusarz <paulslusarz@carfax.com>
2025-03-16 21:52:15 -0500

    more tests converted

--- src/test/java/org/gnubridge/core/bidding/rules/Open1NTTest.java
+++ src/test/java/org/gnubridge/core/bidding/rules/Open1NTTest.java
@@ -1,72 +1,62 @@
 package org.gnubridge.core.bidding.rules;
 
 import static org.gnubridge.core.bidding.Bid.*;
-import junit.framework.TestCase;
 
-import org.gnubridge.core.Hand;
-import org.gnubridge.core.West;
-import org.gnubridge.core.bidding.Auctioneer;
-import org.gnubridge.core.bidding.Bid;
 import org.gnubridge.core.bidding.RPQuizzes;
-import org.gnubridge.core.deck.Clubs;
 
-public class Open1NTTest extends TestCase {
-	public void testOpeningOneNTFirstCall() {
-		Auctioneer a = new Auctioneer(West.i());
-		BiddingRule rule = new Open1NT(a, new Hand("K,2", "A,Q,3", "A,8,6,5,3", "K,J,3"));
-		assertEquals(ONE_NOTRUMP, rule.getBid());
-	}
+public class Open1NTTest extends AbstractBiddingRuleTest<Open1NT> {
 
-	public void testDoNotOpenOneNTWhenResponding() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(ONE_CLUBS);
-		a.bid(PASS);
-		BiddingRule rule = new Open1NT(a, new Hand("K,2", "A,Q,3", "A,8,6,5,3", "K,J,3"));
-		assertEquals(null, rule.getBid());
-	}
+    public void testOpeningOneNTFirstCall() {
+        givenNoPriorBids();
+        andPlayersCards("K,2", "A,Q,3", "A,8,6,5,3", "K,J,3");
+        ruleShouldBid(ONE_NOTRUMP);
+    }
 
-	public void testOpeningOneNTSecondCall() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(PASS);
-		BiddingRule rule = new Open1NT(a, new Hand("K,2", "A,Q,3", "A,8,6,5,3", "K,J,3"));
-		assertEquals(ONE_NOTRUMP, rule.getBid());
-	}
+    public void testDoNotOpenOneNTWhenResponding() {
+        givenBidding(ONE_CLUBS, PASS);
+        andPlayersCards("K,2", "A,Q,3", "A,8,6,5,3", "K,J,3");
+        ruleShouldBid(null);
+    }
 
-	public void testCannotOpenOneNTIfInsufficientHCP() {
-		Auctioneer a = new Auctioneer(West.i());
-		BiddingRule rule = new Open1NT(a, new Hand("K,2", "A,3", "A,8,6,5,3", "K,J,3"));
-		assertEquals(null, rule.getBid());
-	}
+    public void testOpeningOneNTSecondCall() {
+        givenBidding(PASS);
+        andPlayersCards("K,2", "A,Q,3", "A,8,6,5,3", "K,J,3");
+        ruleShouldBid(ONE_NOTRUMP);
+    }
 
-	public void testDoNotOpenOneNTOnImbalancedHand() {
-		Auctioneer a = new Auctioneer(West.i());
-		BiddingRule rule = new Open1NT(a, new Hand("K", "A,Q,3", "A,8,6,5,3", "K,J,3,2"));
-		assertEquals(null, rule.getBid());
-	}
+    public void testCannotOpenOneNTIfInsufficientHCP() {
+        givenNoPriorBids();
+        andPlayersCards("K,2", "A,3", "A,8,6,5,3", "K,J,3");
+        ruleShouldBid(null);
+    }
 
-	public void testCannotOpenOneNTIfHigherBid() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(new Bid(2, Clubs.i()));
-		BiddingRule rule = new Open1NT(a, new Hand("K,2", "A,Q,3", "A,8,6,5,3", "K,J,3"));
-		assertEquals(null, rule.getBid());
-	}
+    public void testDoNotOpenOneNTOnImbalancedHand() {
+        givenNoPriorBids();
+        andPlayersCards("K", "A,Q,3", "A,8,6,5,3", "K,J,3,2");
+        ruleShouldBid(null);
+    }
 
-	public void testRP1() {
-		Auctioneer a = new Auctioneer(West.i());
-		BiddingRule rule = new Open1NT(a, RPQuizzes.Basics.Lesson2.hand1());
-		assertEquals(ONE_NOTRUMP, rule.getBid());
-	}
+    public void testCannotOpenOneNTIfHigherBid() {
+        givenBidding(TWO_CLUBS);
+        andPlayersCards("K,2", "A,Q,3", "A,8,6,5,3", "K,J,3");
+        ruleShouldBid(null);
+    }
 
-	public void testRP5() {
-		Auctioneer a = new Auctioneer(West.i());
-		BiddingRule rule = new Open1NT(a, RPQuizzes.Basics.Lesson2.hand5());
-		assertEquals(ONE_NOTRUMP, rule.getBid());
-	}
+    public void testRP1() {
+        givenNoPriorBids();
+        andPlayersCards(RPQuizzes.Basics.Lesson2.hand1());
+        ruleShouldBid(ONE_NOTRUMP);
+    }
 
-	public void testRP11() {
-		Auctioneer a = new Auctioneer(West.i());
-		BiddingRule rule = new Open1NT(a, RPQuizzes.Basics.Lesson2.hand11());
-		assertEquals(ONE_NOTRUMP, rule.getBid());
-	}
+    public void testRP5() {
+        givenNoPriorBids();
+        andPlayersCards(RPQuizzes.Basics.Lesson2.hand5());
+        ruleShouldBid(ONE_NOTRUMP);
+    }
 
+    public void testRP11() {
+        givenNoPriorBids();
+        andPlayersCards(RPQuizzes.Basics.Lesson2.hand11());
+        ruleShouldBid(ONE_NOTRUMP);
+    }
 }
--- src/test/java/org/gnubridge/core/bidding/rules/OvercallSuitTest.java
+++ src/test/java/org/gnubridge/core/bidding/rules/OvercallSuitTest.java
@@ -1,139 +1,114 @@
 package org.gnubridge.core.bidding.rules;
 
-import static org.gnubridge.core.bidding.Bid.*;
-import junit.framework.TestCase;
-
 import org.gnubridge.core.Hand;
-import org.gnubridge.core.West;
-import org.gnubridge.core.bidding.Auctioneer;
 
-public class OvercallSuitTest extends TestCase {
-	public static final Hand ONE_DIAMONDS_OVERCALL_HAND = new Hand("7,8", "4,3", "A,K,J,9,3,2", "Q,5,4");
-	public static final Hand ONE_HEARTS_OVERCALL_HAND = new Hand("7,8", "A,K,J,9,3,2", "4,3", "Q,5,4");
+import static org.gnubridge.core.bidding.Bid.*;
 
-	public void testOnlyAppliesToTrueOvercalls() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(PASS);
-		OvercallSuit rule = new OvercallSuit(a, ONE_DIAMONDS_OVERCALL_HAND);
-		assertEquals(null, rule.getBid());
+public class OvercallSuitTest extends AbstractBiddingRuleTest<OvercallSuit> {
+    public static final Hand ONE_DIAMONDS_OVERCALL_CARDS = new Hand("7,8", "4,3","A,K,J,9,3,2","Q,5,4");
+    public static final Hand ONE_HEARTS_OVERCALL_CARDS = new Hand("7,8", "A,K,J,9,3,2","4,3","Q,5,4");
 
-		Auctioneer b = new Auctioneer(West.i());
-		b.bid(ONE_CLUBS);
-		b.bid(PASS);
-		rule = new OvercallSuit(b, ONE_DIAMONDS_OVERCALL_HAND);
-		assertEquals(null, rule.getBid());
+    public void testOnlyAppliesToTrueOvercalls() {
+        givenBidding(PASS);
+        andPlayersCards(ONE_DIAMONDS_OVERCALL_CARDS);
+        ruleShouldBid(null);
 
-		Auctioneer c = new Auctioneer(West.i());
-		rule = new OvercallSuit(c, ONE_DIAMONDS_OVERCALL_HAND);
-		assertEquals(null, rule.getBid());
-	}
+        givenBidding(ONE_CLUBS, PASS);
+        andPlayersCards(ONE_DIAMONDS_OVERCALL_CARDS);
+        ruleShouldBid(null);
 
-	public void test1ColorAt10to12SuitOf6() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(ONE_CLUBS);
-		OvercallSuit rule = new OvercallSuit(a, ONE_DIAMONDS_OVERCALL_HAND);
-		assertEquals(ONE_DIAMONDS, rule.getBid());
-	}
+        givenNoPriorBids();
+        andPlayersCards(ONE_DIAMONDS_OVERCALL_CARDS);
+        ruleShouldBid(null);
+    }
 
-	public void test1ColorAt10to12DecentSuitOf5() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(ONE_CLUBS);
-		OvercallSuit rule = new OvercallSuit(a, new Hand("7,8", "K,3,2", "Q,J,9,3,2", "A,5,4"));
-		assertEquals(ONE_DIAMONDS, rule.getBid());
-	}
+    public void test1ColorAt10to12SuitOf6() {
+        givenBidding(ONE_CLUBS);
+        andPlayersCards(ONE_DIAMONDS_OVERCALL_CARDS);
+        ruleShouldBid(ONE_DIAMONDS);
+    }
 
-	public void testGoodSuitOf5ButLessThan10Points() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(ONE_CLUBS);
-		OvercallSuit rule = new OvercallSuit(a, new Hand("7,8", "4,3,2", "A,K,J,9,3", "6,5,4"));
-		assertEquals(null, rule.getBid());
-	}
+    public void test1ColorAt10to12DecentSuitOf5() {
+        givenBidding(ONE_CLUBS);
+        andPlayersCards("7,8", "K,3,2", "Q,J,9,3,2", "A,5,4");
+        ruleShouldBid(ONE_DIAMONDS);
+    }
 
-	public void test1ColorAt10to12PoorSuitOf5() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(ONE_CLUBS);
-		OvercallSuit rule = new OvercallSuit(a, new Hand("7,8", "A,3,2", "K,10,9,3,2", "Q,J,4"));
-		assertEquals(null, rule.getBid());
-	}
+    public void testGoodSuitOf5ButLessThan10Points() {
+        givenBidding(ONE_CLUBS);
+        andPlayersCards("7,8", "4,3,2", "A,K,J,9,3", "6,5,4");
+        ruleShouldBid(null);
+    }
 
-	public void test1DifferentColorAt10to12() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(ONE_CLUBS);
-		OvercallSuit rule = new OvercallSuit(a, ONE_HEARTS_OVERCALL_HAND);
-		assertEquals(ONE_HEARTS, rule.getBid());
-	}
+    public void test1ColorAt10to12PoorSuitOf5() {
+        givenBidding(ONE_CLUBS);
+        andPlayersCards("7,8", "A,3,2", "K,10,9,3,2", "Q,J,4");
+        ruleShouldBid(null);
+    }
 
-	public void testTwoPassesPrecedeRightHandOpponentsOpening() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(PASS);
-		a.bid(PASS);
-		a.bid(ONE_CLUBS);
-		OvercallSuit rule = new OvercallSuit(a, ONE_DIAMONDS_OVERCALL_HAND);
-		assertEquals(ONE_DIAMONDS, rule.getBid());
-	}
+    public void test1DifferentColorAt10to12() {
+        givenBidding(ONE_CLUBS);
+        andPlayersCards(ONE_HEARTS_OVERCALL_CARDS);
+        ruleShouldBid(ONE_HEARTS);
+    }
 
-	public void testOnePassPrecedesRightHandOpponentsOpening() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(PASS);
-		a.bid(ONE_CLUBS);
-		OvercallSuit rule = new OvercallSuit(a, ONE_DIAMONDS_OVERCALL_HAND);
-		assertEquals(ONE_DIAMONDS, rule.getBid());
-	}
+    public void testTwoPassesPrecedeRightHandOpponentsOpening() {
+        givenBidding(PASS, PASS, ONE_CLUBS);
+        andPlayersCards(ONE_DIAMONDS_OVERCALL_CARDS);
+        ruleShouldBid(ONE_DIAMONDS);
+    }
 
-	public void testPickAMoreViableColor() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(ONE_DIAMONDS);
-		OvercallSuit rule = new OvercallSuit(a, new Hand("2", "A,K,J,9,3", "", "10,9,8,7,6,3,2"));
-		assertEquals(ONE_HEARTS, rule.getBid());
-	}
+    public void testOnePassPrecedesRightHandOpponentsOpening() {
+        givenBidding(PASS, ONE_CLUBS);
+        andPlayersCards(ONE_DIAMONDS_OVERCALL_CARDS);
+        ruleShouldBid(ONE_DIAMONDS);
+    }
 
-	public void test13PointsCanBidOnPoor5() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(ONE_CLUBS);
-		OvercallSuit rule = new OvercallSuit(a, new Hand("7,8", "A,K,2", "K,J,9,3,2", "Q,5,4"));
-		assertEquals(ONE_DIAMONDS, rule.getBid());
-	}
+    public void testPickAMoreViableColor() {
+        givenBidding(ONE_DIAMONDS);
+        andPlayersCards("2", "A,K,J,9,3", "", "10,9,8,7,6,3,2");
+        ruleShouldBid(ONE_HEARTS);
+    }
 
-	public void test13PointsCanBidOnPoor5PickMoreViableColor() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(ONE_DIAMONDS);
-		OvercallSuit rule = new OvercallSuit(a, new Hand("A,10,7,8,2", "", "K,J,9,3,2", "Q,5,4"));
-		assertEquals(ONE_SPADES, rule.getBid());
-	}
+    public void test13PointsCanBidOnPoor5() {
+        givenBidding(ONE_CLUBS);
+        andPlayersCards("7,8", "A,K,2", "K,J,9,3,2", "Q,5,4");
+        ruleShouldBid(ONE_DIAMONDS);
+    }
 
-	public void test13PointsCanBid2LevelIfNecessary() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(ONE_SPADES);
-		OvercallSuit rule = new OvercallSuit(a, new Hand("7,8", "A,K", "K,J,9,4,3,2", "Q,5,4"));
-		assertEquals(TWO_DIAMONDS, rule.getBid());
-	}
+    public void test13PointsCanBidOnPoor5PickMoreViableColor() {
+        givenBidding(ONE_DIAMONDS);
+        andPlayersCards("A,10,7,8,2", "", "K,J,9,3,2", "Q,5,4");
+        ruleShouldBid(ONE_SPADES);
+    }
 
-	public void test13PointsCanBid2LevelForGood5() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(ONE_SPADES);
-		OvercallSuit rule = new OvercallSuit(a, new Hand("7,8,2", "A,K", "K,Q,10,4,3", "Q,5,4"));
-		assertEquals(TWO_DIAMONDS, rule.getBid());
-	}
+    public void test13PointsCanBid2LevelIfNecessary() {
+        givenBidding(ONE_SPADES);
+        andPlayersCards("7,8", "A,K", "K,J,9,4,3,2", "Q,5,4");
+        ruleShouldBid(TWO_DIAMONDS);
+    }
 
-	public void test13PointsCanotBid2LevelForDecent5() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(ONE_SPADES);
-		OvercallSuit rule = new OvercallSuit(a, new Hand("7,8,2", "A,K", "Q,J,10,4,3", "K,5,4"));
-		assertEquals(null, rule.getBid());
-	}
+    public void test13PointsCanBid2LevelForGood5() {
+        givenBidding(ONE_SPADES);
+        andPlayersCards("7,8,2", "A,K", "K,Q,10,4,3", "Q,5,4");
+        ruleShouldBid(TWO_DIAMONDS);
+    }
 
-	public void test16PointsCanBid2LevelForAny5LongSuit() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(ONE_SPADES);
-		OvercallSuit rule = new OvercallSuit(a, new Hand("K,8,2", "A,K", "K,J,9,4,3", "Q,5,4"));
-		assertEquals(TWO_DIAMONDS, rule.getBid());
-	}
+    public void test13PointsCanotBid2LevelForDecent5() {
+        givenBidding(ONE_SPADES);
+        andPlayersCards("7,8,2", "A,K", "Q,J,10,4,3", "K,5,4");
+        ruleShouldBid(null);
+    }
 
-	public void test16PointsBidAtLowestLevelPossibleAny5LongSuit() {
-		Auctioneer a = new Auctioneer(West.i());
-		a.bid(ONE_CLUBS);
-		OvercallSuit rule = new OvercallSuit(a, new Hand("K,8,2", "A,K", "K,J,9,4,3", "Q,5,4"));
-		assertEquals(ONE_DIAMONDS, rule.getBid());
-	}
+    public void test16PointsCanBid2LevelForAny5LongSuit() {
+        givenBidding(ONE_SPADES);
+        andPlayersCards("K,8,2", "A,K", "K,J,9,4,3", "Q,5,4");
+        ruleShouldBid(TWO_DIAMONDS);
+    }
 
+    public void test16PointsBidAtLowestLevelPossibleAny5LongSuit() {
+        givenBidding(ONE_CLUBS);
+        andPlayersCards("K,8,2", "A,K", "K,J,9,4,3", "Q,5,4");
+        ruleShouldBid(ONE_DIAMONDS);
+    }
 }
